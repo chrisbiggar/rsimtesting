35eb265b1b4601d34e4f27a404e5c00887f5b136
Chris Biggar <chrisbggr@gmail.com>
2013-07-07 11:25:55 -0400

    02

--- car.py
+++ car.py
@@ -3,6 +3,7 @@ Created on 2013-07-04
 
 @author: chris_000
 '''
+import math
 import pyglet
 from pyglet.window import key
 from pyglet.sprite import Sprite
@@ -18,28 +19,52 @@ class Car(Sprite):
         self.vely = 0.0
         self.velz = 0.0
         
+        self.speed = 0
+        self.angle = 0
+        
         self.dx = 25
-        self.friction = 2.
+        
+        self.FRICTION = 2.
+        self.POWER = 0.5
+        self.MAXSPEED = 10
+        
         '''sprite'''
         img = pyglet.image.load('res/car.png')
         Sprite.__init__(self, img)
+        #self.batch = sim.batch
+        self.rotation = 90
         
     def update(self):
         #print "update"
         if self.sim.keys[key.UP]:
             print "up"
-            self.vely = 25
-        if self.sim.keys[key.DOWN]:
+            self.speed = 10
+        elif self.sim.keys[key.DOWN]:
             print "down"
-            self.vely = -25
+            self.speed = -10
+        else:
+            self.speed = 0
+        if self.sim.keys[key.LEFT]:
+            self.angle += 22.5
+            self.rotation -= 22.5
+        if self.sim.keys[key.RIGHT]:
+            self.angle -= 22.5
+            self.rotation += 22.5
             
-        if self.vely > 0:
-            self.vely -= self.friction
+        #if self.vely > 0:
+            #self.vely -= self.friction
         
         '''if self.vely < 0:
             self.vely += self.friction
         elif self.vely > 0:
             self.vely -= self.friction'''
+            
+        #self.rotation = self.angle + 90
+        
+        scale_x = math.cos(math.radians(self.angle))
+        scale_y = math.sin(math.radians(self.angle))
+        self.velx = self.speed * scale_x
+        self.vely = self.speed * scale_y
         
         self.x += self.velx
         self.y += self.vely
--- racedot.py
+++ racedot.py
@@ -8,39 +8,37 @@ import pyglet
 from pyglet.window import key
 import car
 
-class RaceSim(pyglet.window.Window):
-    def __init__(self):
-        super(RaceSim, self).__init__()
+class Window(pyglet.window.Window):
+    def __init__(self, sim):
+        super(Window, self).__init__()
         self.set_caption("Car Physics")
-        self.batch = pyglet.graphics.Batch()
-        self.keys = key.KeyStateHandler()
-        self.push_handlers(self.keys)
+        self.set_size(800,600)
+        self.sim = sim
         
     def on_draw(self):
         self.clear()
+        self.sim.draw()
+
+class RaceSim(object):
+    def __init__(self):
+        self.window = Window(self)
+        self.batch = pyglet.graphics.Batch()
+        self.keys = key.KeyStateHandler()
+        self.window.push_handlers(self.keys)
+        self.initsim()
+        pyglet.clock.schedule_interval(self.update, 1/60.)
+        
+    def initsim(self):
+        self.car = car.Car(self)
+        self.car.x = 200
+        self.car.y = 200
+        self.car.batch = self.batch
+    
+    def update(self, dt):
+        self.car.update()
+        
+    def draw(self):
         self.batch.draw()
 
 sim = RaceSim()
-'''init res'''
-car = car.Car(sim)
-car.batch = sim.batch
-car.x = 200
-car.y = 200
-
-'''@win.event
-def on_key_press(symbol, modifiers):
-    pass'''
-        
-def update(self):
-    car.update()
-    
-    '''if sim.keys[key.DOWN]:
-        print "space"'''
-        
-'''@win.event
-def on_draw():
-    win.clear()
-    batch.draw()'''
-    
-pyglet.clock.schedule_interval(update, 1/60.)
 pyglet.app.run()
